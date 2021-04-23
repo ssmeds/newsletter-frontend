27482598818f7b2f91f42acde305f48952494ad4
ssmeds <stina_smeds@hotmail.com>
2021-04-23 10:03:31 +0200

    uploading to work at another computer

---
+++ script.js
@@ -0,0 +1,113 @@
+if (localStorage.getItem("loggedInUser") != null) {
+  let user = JSON.parse(localStorage.getItem("loggedInUser"));
+  profile(user);
+} else {
+  logIn();
+  register();
+}
+
+// logIn();
+
+
+function logIn() {
+
+
+  let loginForm = `<div>
+  <input type="text" name="userName" id="userName" placeholder="Användarnamn">
+  <input type="password" name="userPass" id="password" placeholder="Lösenord">
+  <button id="loginBtn">Logga in</button></div>`
+
+
+  document.getElementById("container").insertAdjacentHTML("afterbegin", loginForm);
+  document.getElementById("loginBtn").addEventListener("click", function () {
+    let userName = document.getElementById("userName").value;
+    let userPass = document.getElementById("password").value;
+
+    let user = {
+      userName: userName,
+      userPass: userPass,
+      userId: "",
+      newsletter: "false"
+    }
+
+    console.log('klick på knappen för att logga in');
+    console.log(user);
+
+    fetch('http://localhost:3001/users/login', {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(user)
+      })
+      .then(res => res.json())
+      .then(user => {
+        console.log('Success:', user);
+        if (user != "Terror") {
+          let userToLS = {
+            userName: user.userName,
+            userId: user.userId
+          }
+          console.log(userToLS);
+          localStorage.setItem('loggedInUser', JSON.stringify(userToLS));
+          console.log("användaren sparad till LS");
+          console.log("user till profile", user);
+          profile(user);
+        } else {
+          console.log("Något gick fel");
+        }
+      })
+
+  })
+}
+
+function register() {
+  let registerForm = `<form action="http://localhost:3001/users/add" method="post">
+  <label for="userName">Användarnamn: </label><input type="text" name="userName" id="userName">
+  <label for="password">Lösenord: </label><input type="text" name="password" id="userPass">
+  <button type="submit" id="register">Registrera dig</button>
+  <a href="http://localhost:3001/">Registera dig</a>
+</form>`
+
+  document.getElementById("container").insertAdjacentHTML("beforeend", registerForm);
+  document.getElementById("register").addEventListener("click", function () {
+    console.log('klick på knappen för att registrera sig');
+  })
+}
+
+function profile(user) {
+  console.log(user.userName);
+  fetch(`http://localhost:3001/users/list/${user.userName}`)
+    .then(res => res.json())
+    .then(data => {
+      let userData = JSON.parse(data);
+      console.log("newsletter status", userData);
+      let text;
+      let buttonText;
+      if (userData == "" || userData == false) {
+        text = "Nej";
+        buttonText = "Vill du ha mitt nyhetsbrev?"
+      } else {
+        text = "Ja";
+        buttonText = "Vill du avsluta din prenumeration?"
+      }
+
+
+      let profileView = `<div>
+      <h2>Hej ${user.userName}</h2>
+      <div>Nyhetsbrev: <span>${text}</span></div>
+      <a href="http://localhost:3001/users/newsletter/${user.userName}"><button id="newsBtn">${buttonText}</button></a>
+    </div>`
+      document.getElementById("container").innerHTML = profileView;
+
+
+      // document.getElementById("newsBtn").addEventListener("click", function () {
+      //   fetch(`http://localhost:3001/users/list/:${user.userName}`)
+      //     .then(res => res.json())
+      //     .then(data => {
+      //       let userData = JSON.parse(data);
+      //       console.log(userData);
+      //     })
+      // })
+    })
+}
